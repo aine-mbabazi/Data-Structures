bdd77e49efce42c5632204d5c12c7ebc9286f6ca
aine-mbabazi <patainembabazi@gmail.com>
2024-03-31 14:11:17 +0300

    A file with work on tuples

---
+++ tuple.py
@@ -0,0 +1,17 @@
+tuple1 = (100,200,300,400,500)
+tuple2 = (600,700,800,900,1000)
+# tuple length
+print(len(tuple1))
+
+# tuple concatenation
+tuple3 = tuple1+tuple2
+print(tuple3)
+
+# Checking for an element in a tuple
+print(tuple1[1])
+
+# tuple repition
+print(tuple1*2)
+
+# iteration(multiplies each element in the tuple)
+for a in tuple2 : print(a*2)
