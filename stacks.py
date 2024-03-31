5f1710dd42acee9a2ca2ddd655804b6454b1feba
aine-mbabazi <patainembabazi@gmail.com>
2024-03-31 14:13:21 +0300

    Added a file on stacks

---
+++ stacks.py
@@ -0,0 +1,19 @@
+# creating a new stack
+stack = []
+
+# pushing elements to a stack
+stack.append("lovelace")
+stack.append("AnitaB")
+stack.append("Adalab")
+print(stack)
+
+# pop
+stack.pop()
+print(stack.pop())
+print(stack)
+
+# peek
+stack[-1]
+
+# stack2
+stack2 = []
